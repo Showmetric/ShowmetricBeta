bd331363645e2e4b0617a0ee84ead3866651b9ee
RajalakshmiC <rajalakshmi@showmetric.co>
2016-10-24 15:49:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -193,6 +193,17 @@ router.use(function (req, res, next) {
 });
 
 // launch ======================================================================
+var child = new (forever.Monitor)('batchJobs.js', {
+    max: 3,
+    silent: true,
+    logFile: '/batchLog',
+});
+
+child.on('exit', function () {
+    console.log('your-filename.js has exited after 3 restarts');
+});
+
+child.start();
 app.listen(port);
 /*https.createServer({
     key: fs.readFileSync('./key.pem', 'utf8'),
