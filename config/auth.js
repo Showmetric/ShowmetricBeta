ac89963c20ac6766903894bfa7af814a4376b99a
Rajeshkumar V <Rajeshkumar V>
2016-10-31 14:41:55 +0530

    Callback URL changes

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth': {
         clientID: '1716895671912901', // your App ID
         clientSecret: 'e5f9e0828dc78c548ddbc3ac9e3658fd', // your App Secret
-        callbackURL: 'https://datapoolt.co/auth/facebook/callback',
+        callbackURL: 'https://uat.datapoolt.co/auth/facebook/callback',
         scope: 'manage_pages,read_insights',
         profileFields: ["emails", "displayName"],
         site:'https://www.facebook.com/dialog/',
@@ -62,7 +62,7 @@ module.exports = {
     'googleAuth': {
         clientID: '697605351302-cm90a20idvq2gcu3qju0oaab88ik6peg.apps.googleusercontent.com',
         clientSecret: 'DkUY5XrdcWDQM_7tEI9xNAC6',
-        callbackURL: 'https://datapoolt.co/auth/google/callback',
+        callbackURL: 'https://uat.datapoolt.co/auth/google/callback',
         localCallingURL: '/api/v1/auth/google',
         localCallbackURL: '/auth/google/callback',
         site: 'https://accounts.google.com/o/',
@@ -79,7 +79,7 @@ module.exports = {
     'googleAdwordsAuth':{
         clientID: '870217049857-gm1938i5vr53l4og5m5fmp1fjprbe7af.apps.googleusercontent.com',
         clientSecret: '1ZtXnW143hAuBAmZtj0i-rzC',
-        callbackURL: 'https://datapoolt.co/auth/adwords/callback',
+        callbackURL: 'https://uat.datapoolt.co/auth/adwords/callback',
         scope:'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/adwords',
         state: '5$55#',
         managerClientId:'374-579-6439',
@@ -97,7 +97,7 @@ module.exports = {
     'instagramAuth': {
         clientID: '222690d755ba41d0a0defad5e19eceaf',
         clientSecret: '51856a68d3b14b9f85d755bc615986e8',
-        callbackURL: 'https://datapoolt.co/auth/instagram/callback',
+        callbackURL: 'https://uat.datapoolt.co/auth/instagram/callback',
         scope : ['basic'],
         authorizationUrl:'https://api.instagram.com/oauth/authorize',
         accessTokenUrl:'https://api.instagram.com/oauth/access_token',
@@ -112,7 +112,7 @@ module.exports = {
         site            : 'https://www.facebook.com/dialog/',
         tokenPath       : 'https://graph.facebook.com/oauth/access_token',
         authorizationPath: 'oauth',
-        redirect_uri : 'https://datapoolt.co/auth/facebookads/callback',
+        redirect_uri : 'https://uat.datapoolt.co/auth/facebookads/callback',
         scope : 'email,manage_pages,read_insights,ads_read,ads_management',
         state : '4234#',
         localCallingURL: '/api/auth/facebookads',
@@ -286,26 +286,26 @@ module.exports = {
     'mailChimp':{
         clientID        : '236023092083',
         clientSecret    : '2aa7fd91868924efd542767bc65b2ec9',
-        redirect_uri : 'https://datapoolt.co/auth/mailchimp/callback/',
+        redirect_uri : 'https://uat.datapoolt.co/auth/mailchimp/callback/',
         authorizationUrl:"https://login.mailchimp.com/oauth2/authorize",
         accessTokenUrl:"https://login.mailchimp.com/oauth2/token",
         base_uri:"https://login.mailchimp.com/oauth2/",
         metadata:"https://login.mailchimp.com/oauth2/metadata",
-        apiKey:"5b907c6f3b2f00b07270d38b709a8106-us13"
+        apiKey:"6bdce0374cf2724449d5d79454896415-us13"
 
     },
 
     'pinterest':{
         clientID        : '4850584692457482509',
         clientSecret    : '93dbedd44181a79569f40c90bd9c6d1eb04c80ec8016444c84bb53c0901f3092',
-        redirect_uri : 'https://datapoolt.co/auth/pinterest/callback',
+        redirect_uri : 'https://uat.datapoolt.co/auth/pinterest/callback',
         scope:'read_public,read_relationships'
     },
 
     'linkedIn':{
         clientID :'81665zttd0yozd',
         clientSecret :'KUlhe3rR1ZoV3V2o',
-        redirect_uri:'https://datapoolt.co/auth/linkedin/callback',
+        redirect_uri:'https://uat.datapoolt.co/auth/linkedin/callback',
         scope : [["r_basicprofile","r_emailaddress","rw_company_admin"]],
         tokenPath:'https://www.linkedin.com/oauth/v2/accessToken',
         site:'https://www.linkedin.com/oauth/',
