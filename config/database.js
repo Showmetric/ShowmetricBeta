e12ab4727e93f80c589be72bdc7d75c5016d0aa9
RajalakshmiC <rajalakshmi@showmetric.co>
2016-11-18 19:25:03 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,6 +6,7 @@ module.exports = {
     //'url': 'mongodb://admin:admin@localhost:27017/datapoolt_CustomFusions' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     //'url': 'mongodb://admin:admin@ds015334.mlab.com:15334/datapoolt15062016'
     'url':'mongodb://datapoolt:datapooltUat@mongo-uat-1.1b0da96b.cont.dockerapp.io/DatapooltUat'
+    
     //'url':'mongodb://datapoolt:datapooltUat@mongo-uat/DatapooltUat'
     //'url': 'mongodb://admin:admin@ds149567.mlab.com:49567/datapoolt_uat'
 
